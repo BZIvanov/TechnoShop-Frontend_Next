6d401704bd3b514c4530978a18df9a8d3b83d7fb
BZIvanov <biser.z.ivanov@gmail.com>
2020-11-26 16:45:24 +0200

    react-toastify setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
@@ -8,6 +10,7 @@ const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
