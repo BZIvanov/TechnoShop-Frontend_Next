141888f41649b71bbe5ec0c4e3e732fa3e2eab69
BZIvanov <biser.z.ivanov@gmail.com>
2020-11-27 17:48:39 +0200

    registration complete

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
+import RegisterComplete from './pages/auth/RegisterComplete';
 import Header from './components/nav/Header';
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route exact path='/register/complete' component={RegisterComplete} />
       </Switch>
     </>
   );
