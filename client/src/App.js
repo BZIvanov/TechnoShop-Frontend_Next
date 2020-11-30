1e5ce268da6074ac2d6a7d11f166b881bcae3cf7
BZIvanov <biser.z.ivanov@gmail.com>
2020-11-30 13:10:55 +0200

    handle forgot user password

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import RegisterComplete from './pages/auth/RegisterComplete';
+import ForgotPassword from './pages/auth/ForgotPassword';
 import Header from './components/nav/Header';
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
@@ -39,6 +40,7 @@ const App = () => {
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
         <Route exact path='/register/complete' component={RegisterComplete} />
+        <Route exact path='/forgot/password' component={ForgotPassword} />
       </Switch>
     </>
   );
