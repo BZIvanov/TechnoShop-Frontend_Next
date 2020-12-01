19c00b9ee71ce22aac008a9ddefa6c95b7048e08
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-01 21:06:46 +0200

    send user token to the server on register complete

--- client/src/pages/auth/RegisterComplete.js
+++ client/src/pages/auth/RegisterComplete.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
+import { useDispatch } from 'react-redux';
+import { createOrUpdateUser } from '../../functions/auth';
 
 const RegisterComplete = ({ history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     setEmail(window.localStorage.getItem('emailForRegistration'));
@@ -35,7 +39,22 @@ const RegisterComplete = ({ history }) => {
         await user.updatePassword(password);
         const idTokenResult = await user.getIdTokenResult();
 
-        console.log('user', user, 'idTokenResult', idTokenResult);
+        createOrUpdateUser(idTokenResult.token)
+          .then((res) => {
+            dispatch({
+              type: 'LOGGED_IN_USER',
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data._id,
+              },
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
 
         history.push('/');
       }
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,11 @@
 const User = require('../models/user');
 
 exports.createOrUpdateUser = async (req, res) => {
-  const { name, picture, email } = req.user;
+  const { picture, email } = req.user;
 
   const user = await User.findOneAndUpdate(
     { email },
-    { name, picture },
+    { name: email.split('@')[0], picture },
     { new: true }
   );
 
@@ -15,7 +15,7 @@ exports.createOrUpdateUser = async (req, res) => {
   } else {
     const newUser = await new User({
       email,
-      name,
+      name: email.split('@')[0],
       picture,
     }).save();
     console.log('USER CREATED', newUser);
