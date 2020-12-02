4c1b1db2ddcd5f5a5fcb3bd1e24e989dc0acbf04
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-02 20:30:02 +0200

    user protected route and redirect based on user's role

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import RegisterComplete from './pages/auth/RegisterComplete';
 import ForgotPassword from './pages/auth/ForgotPassword';
+import History from './pages/user/History';
 import Header from './components/nav/Header';
+import UserRoute from './components/routes/UserRoute';
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
 import { currentUser } from './functions/auth';
@@ -49,6 +51,7 @@ const App = () => {
         <Route exact path='/register' component={Register} />
         <Route exact path='/register/complete' component={RegisterComplete} />
         <Route exact path='/forgot/password' component={ForgotPassword} />
+        <UserRoute exact path='/user/history' component={History} />
       </Switch>
     </>
   );
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -20,6 +20,14 @@ const Login = ({ history }) => {
 
   const dispatch = useDispatch();
 
+  const roleBasedRedirect = (res) => {
+    if (res.data.role === 'admin') {
+      history.push('/admin/dashboard');
+    } else {
+      history.push('/user/history');
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
@@ -41,10 +49,9 @@ const Login = ({ history }) => {
               _id: res.data._id,
             },
           });
+          roleBasedRedirect(res);
         })
         .catch((err) => console.log(err));
-
-      history.push('/');
     } catch (error) {
       console.log(error);
       toast.error(error.message);
@@ -70,10 +77,9 @@ const Login = ({ history }) => {
                 _id: res.data._id,
               },
             });
+            roleBasedRedirect(res);
           })
           .catch((err) => console.log(err));
-
-        history.push('/');
       })
       .catch((err) => {
         console.log(err);
