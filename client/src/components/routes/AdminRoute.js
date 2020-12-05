5bfbeb69912cd2fc6ceba312b27db77d4713177c
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-05 22:14:39 +0200

    admin route to limit access to admin only

---
+++ client/src/components/routes/AdminRoute.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import LoadingToRedirect from './LoadingToRedirect';
+import { currentAdmin } from '../../functions/auth';
+
+const AdminRoute = (props) => {
+  const { user } = useSelector((state) => ({ ...state }));
+  const [ok, setOk] = useState(false);
+
+  useEffect(() => {
+    if (user && user.token) {
+      currentAdmin(user.token)
+        .then((res) => {
+          console.log('CURRENT ADMIN RES', res);
+          setOk(true);
+        })
+        .catch((err) => {
+          console.log('ADMIN ROUTE ERR', err);
+          setOk(false);
+        });
+    }
+  }, [user]);
+
+  return ok ? <Route {...props} /> : <LoadingToRedirect />;
+};
+
+export default AdminRoute;
