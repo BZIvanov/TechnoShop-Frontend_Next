d71bc59d2ec17eb17c4cfe0c8e76c3925827e817
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-10 13:07:09 +0200

    get category's subcategories route and controller

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -41,3 +41,11 @@ exports.remove = async (req, res) => {
     res.status(400).send('Category delete failed');
   }
 };
+
+exports.getSubs = (req, res) => {
+  Sub.find({ parent: req.params._id }).exec((err, subs) => {
+    if (err) console.log(err);
+
+    res.json(subs);
+  });
+};
