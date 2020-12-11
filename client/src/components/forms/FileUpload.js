5236192fd41a3f3fc100a55f6690d94d03047d12
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-11 15:34:12 +0200

    react-image-file-resizer setup

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,6 +1,25 @@
+import Resizer from 'react-image-file-resizer';
+
 const FileUpload = () => {
   const fileUploadAndResize = (e) => {
-    console.log(e.target.files);
+    const files = e.target.files;
+
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          'JPEG',
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          'base64'
+        );
+      }
+    }
   };
 
   return (
--- client/src/pages/admin/product/ProductCreate.js
+++ client/src/pages/admin/product/ProductCreate.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { createProduct } from '../../../functions/product';
 import ProductCreateForm from '../../../components/forms/ProductCreateForm';
 import { getCategories, getCategorySubs } from '../../../functions/category';
+import FileUpload from '../../../components/forms/FileUpload';
 
 const initialState = {
   title: '',
