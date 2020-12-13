3b4ede8df58ca4b24ff919f4a8b3ce094adeb6fb
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-13 22:25:22 +0200

    moved products from the dashboard to the respective route with all products

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -11,7 +11,8 @@ const AdminProductCard = ({ product }) => {
         <img
           src={images && images.length ? images[0].url : ''}
           style={{ height: '150px', objectFit: 'cover' }}
-          className='m-2'
+          className='p-1'
+          alt='product preview'
         />
       }
     >
