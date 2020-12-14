2055226dbd455cc071b7a2a55cfc20ab98e66759
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-14 12:54:45 +0200

    default image for products without image

--- client/src/components/cards/AdminProductCard.js
+++ client/src/components/cards/AdminProductCard.js
@@ -1,22 +1,33 @@
 import { Card } from 'antd';
+import laptop from '../../images/laptop.png';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 
 const { Meta } = Card;
 
 const AdminProductCard = ({ product }) => {
   const { title, description, images } = product;
 
+  const formattedDescription =
+    description && description.length > 40
+      ? description.substring(0, 40) + '...'
+      : description;
+
   return (
     <Card
       cover={
         <img
-          src={images && images.length ? images[0].url : ''}
+          src={images && images.length ? images[0].url : laptop}
           style={{ height: '150px', objectFit: 'cover' }}
           className='p-1'
           alt='product preview'
         />
       }
+      actions={[
+        <EditOutlined className='text-warning' />,
+        <DeleteOutlined className='text-danger' />,
+      ]}
     >
-      <Meta title={title} description={description} />
+      <Meta title={title} description={formattedDescription} />
     </Card>
   );
 };
