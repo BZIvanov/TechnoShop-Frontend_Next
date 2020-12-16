1f94bea9058909836d37d9240fad76e376128bb9
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-16 16:47:38 +0200

    display product details

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,26 +1,49 @@
-import { Card } from 'antd';
+import { Card, Tabs } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Laptop from '../../images/laptop.png';
+import ProductListItems from './ProductListItems';
 
-const { Meta } = Card;
+const { TabPane } = Tabs;
 
 const SingleProduct = ({ product }) => {
-  const { title, description, images } = product;
+  const { title, images, description } = product;
 
   return (
     <>
       <div className='col-md-7'>
-        <Carousel showArrows={true} autoPlay infiniteLoop>
-          {images &&
-            images.map((i) => (
+        {images && images.length ? (
+          <Carousel showArrows={true} autoPlay infiniteLoop>
+            {images.map((i) => (
               <img src={i.url} key={i.public_id} alt='product-preview' />
             ))}
-        </Carousel>
+          </Carousel>
+        ) : (
+          <Card
+            cover={
+              <img
+                src={Laptop}
+                className='mb-3 card-image'
+                alt='default-preview'
+              />
+            }
+          ></Card>
+        )}
+
+        <Tabs type='card'>
+          <TabPane tab='Description' key='1'>
+            {description}
+          </TabPane>
+          <TabPane tab='More' key='2'>
+            Call use on xxxx xxx xxx to learn more about this product.
+          </TabPane>
+        </Tabs>
       </div>
 
       <div className='col-md-5'>
+        <h1 className='bg-info p-3'>{title}</h1>
         <Card
           actions={[
             <>
@@ -32,10 +55,7 @@ const SingleProduct = ({ product }) => {
             </Link>,
           ]}
         >
-          <Meta title={title} description={description} />
-          <p>
-            price/category/subs/shipping/color/brand/quantity available/sold
-          </p>
+          <ProductListItems product={product} />
         </Card>
       </div>
     </>
