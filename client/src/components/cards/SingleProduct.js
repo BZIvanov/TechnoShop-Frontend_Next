7a39abdf067a7ea7192de3af78b232652f05f722
BZIvanov <biser.z.ivanov@gmail.com>
2020-12-16 12:13:21 +0200

    react-responsive-carousel package for product images

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,15 +1,24 @@
 import { Card } from 'antd';
 import { Link } from 'react-router-dom';
 import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const { Meta } = Card;
 
 const SingleProduct = ({ product }) => {
-  const { title, description } = product;
+  const { title, description, images } = product;
 
   return (
     <>
-      <div className='col-md-7'>image carousel</div>
+      <div className='col-md-7'>
+        <Carousel showArrows={true} autoPlay infiniteLoop>
+          {images &&
+            images.map((i) => (
+              <img src={i.url} key={i.public_id} alt='product-preview' />
+            ))}
+        </Carousel>
+      </div>
 
       <div className='col-md-5'>
         <Card
