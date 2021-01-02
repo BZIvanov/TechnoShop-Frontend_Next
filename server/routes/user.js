5592e4b5c3635798a83de2d8b73860c74eef8236
BZIvanov <biser.z.ivanov@gmail.com>
2021-01-02 14:54:13 +0200

    get orders controller and render them on the client

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,6 +7,7 @@ const {
   saveAddress,
   applyCouponToUserCart,
   createOrder,
+  orders,
 } = require('../controllers/user');
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.delete('/user/cart', authCheck, emptyCart);
 router.post('/user/address', authCheck, saveAddress);
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart);
 router.post('/user/order', authCheck, createOrder);
+router.get('/user/orders', authCheck, orders);
 
 module.exports = router;
