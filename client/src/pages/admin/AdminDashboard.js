908caef8a2a52917eba40a70900431c0c260815e
BZIvanov <biser.z.ivanov@gmail.com>
2021-01-02 21:53:22 +0200

    update order status controller and all render orders for the admin

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,6 +1,34 @@
+import { useState, useEffect, useCallback } from 'react';
 import AdminNav from '../../components/nav/AdminNav';
+import { useSelector } from 'react-redux';
+import { getOrders, changeStatus } from '../../functions/admin';
+import Orders from '../../components/order/Orders';
+import { toast } from 'react-toastify';
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([]);
+
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const loadOrders = useCallback(
+    () =>
+      getOrders(user.token).then((res) => {
+        setOrders(res.data);
+      }),
+    [user.token]
+  );
+
+  useEffect(() => {
+    loadOrders();
+  }, [loadOrders]);
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token).then(() => {
+      toast.success('Status updated');
+      loadOrders();
+    });
+  };
+
   return (
     <div className='container-fluid'>
       <div className='row'>
@@ -8,8 +36,9 @@ const AdminDashboard = () => {
           <AdminNav />
         </div>
 
-        <div className='col'>
+        <div className='col-md-10'>
           <h4>Admin Dashboard</h4>
+          <Orders orders={orders} handleStatusChange={handleStatusChange} />
         </div>
       </div>
     </div>
