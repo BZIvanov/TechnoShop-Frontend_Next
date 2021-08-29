1989f346ee044bad06c32a41bb1a8315e70469a3
BZIvanov <ivanov.biser@gmx.com>
2021-08-30 00:55:13 +0300

    redux store refactor

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
+import { store } from './store';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import 'antd/dist/antd.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
-
-const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
