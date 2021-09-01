845a85bd136df650d842cb59076b05b8c5909857
BZIvanov <ivanov.biser@gmx.com>
2021-09-01 11:11:40 +0300

    bodyParser fix

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
+const { readdirSync } = require('fs');
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
-require('dotenv').config();
-const { readdirSync } = require('fs');
 
 const app = express();
 
 mongoose
-  .connect(process.env.DATABASE, {
+  .connect(process.env.DATABASE_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
@@ -19,7 +18,7 @@ mongoose
   .catch((err) => console.log('DB CONNECTION ERR', err));
 
 app.use(morgan('dev'));
-app.use(bodyParser.json({ limit: '2mb' }));
+app.use(express.json({ limit: '2mb' }));
 app.use(cors());
 
 readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
