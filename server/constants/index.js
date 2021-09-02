f5106ea80863b76ba2b80d27aa81f6e4cfd9a7b2
BZIvanov <ivanov.biser@gmx.com>
2021-09-02 10:30:36 +0300

    user schema refactor

---
+++ server/constants/index.js
@@ -0,0 +1,14 @@
+const USER_ROLES = {
+  USER: 'user',
+  ADMIN: 'admin',
+};
+
+const MODELS = {
+  USER: 'User',
+  PRODUCT: 'Product',
+};
+
+module.exports = {
+  USER_ROLES,
+  MODELS,
+};
--- server/server.js
+++ server/server.js
@@ -1,21 +1,12 @@
 const { readdirSync } = require('fs');
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const app = express();
+require('./database');
 
-mongoose
-  .connect(process.env.DATABASE_URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('DB CONNECTED'))
-  .catch((err) => console.log('DB CONNECTION ERR', err));
+const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json({ limit: '2mb' }));
@@ -24,5 +15,4 @@ app.use(cors());
 readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
 
 const port = process.env.PORT || 8000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
