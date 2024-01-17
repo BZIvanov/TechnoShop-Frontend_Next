10ef7481c15efe56b77fd778c5e283a4b4a8518e
BZIvanov <ivanov.biser@gmx.com>
2024-01-17 17:38:48 +0200

    auth actions

--- schemas/index.ts
+++ schemas/index.ts
@@ -6,3 +6,11 @@ export const LoginSchema = z.object({
     .string()
     .min(8, { message: 'Password should have at least 8 characters' }),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({ message: 'Email is required' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password should have at least 8 characters' }),
+  name: z.string().min(1, { message: 'Name is required' }),
+});
